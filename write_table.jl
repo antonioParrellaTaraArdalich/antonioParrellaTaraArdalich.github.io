c1bf2286858eb764b2a1d1f17c0643ff99eadf8a
antonioParrella <parrella17@gmail.com>
2023-12-03 23:28:30 +1030

    thesis

--- write_table.jl
+++ write_table.jl
@@ -5,7 +5,7 @@ guest_list = XLSX.readxlsx("guestListClean.xlsx")["Sheet1"][:][2:end,3]
 total_people = length(guest_list)
 names = guest_list .|> String 
 links = Vector{String}(undef, total_people)
-links .= "antonioandtara.co"
+links .= "httpsantonioandtara.co"
 
 function table_row(name::String, link::String)
     row = "<tr>\n   <td><a href = " * link * ">" * name * "</a></td>\n</tr>\n"
